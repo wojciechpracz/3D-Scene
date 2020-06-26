35739f890b5d32f0b2b15b118d928ceedb7f47b0
wojtek0802 <pracz.wojciech@gmail.com>
2020-06-26 18:28:09 +0200

    deleted unused method

--- grafika2/Matrix.cs
+++ grafika2/Matrix.cs
@@ -29,19 +29,6 @@ namespace grafika2
             return v;
         }
 
-        public static void MultiplyMatrixVector(Vec3D i, Vec3D output, Matrix m)
-        {
-            output.x = i.x * m.matrix[0, 0] + i.y * m.matrix[1, 0] + i.z * m.matrix[2, 0] + m.matrix[3, 0];
-            output.y = i.x * m.matrix[0, 1] + i.y * m.matrix[1, 1] + i.z * m.matrix[2, 1] + m.matrix[3, 1];
-            output.z = i.x * m.matrix[0, 2] + i.y * m.matrix[1, 2] + i.z * m.matrix[2, 2] + m.matrix[3, 2];
-            float w = i.x * m.matrix[0, 3] + i.y * m.matrix[1, 3] + i.z * m.matrix[2, 3] + m.matrix[3, 3];
-
-            if (w != 0.0f)
-            {
-                output.x /= w; output.y /= w; output.z /= w;
-            }
-        }
-
         public static Matrix Matrix_MakeIdentity()
         {
             Matrix matrix = new Matrix(4);
